a0301aba97c83dd95033189482b7aed5f540f31c
Harshkaushik14 <kaushikharsh86@gmail.com>
2024-06-08 15:55:51 +0530

    announcemet bar added

--- app/page.tsx
+++ app/page.tsx
@@ -10,13 +10,17 @@ import Approach from "@/components/Approach";
 import Experience from "@/components/Experience";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
+import AnnouncementBar from "@/components/AnnouncementBar";
 
 const Home = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
+        <AnnouncementBar message=" Now offering freelancing services! Contact me today." />
+
         <FloatingNav navItems={navItems} />
         <Hero />
+
         <Grid />
         <RecentProjects />
         <Clients />
---
+++ components/AnnouncementBar.tsx
@@ -0,0 +1,50 @@
+// components/AnnouncementBar.tsx
+import Image from "next/image";
+import React from "react";
+import LaptopIcon from "../public/laptpop.svg";
+
+
+interface AnnouncementBarProps {
+  message: string;
+}
+
+const AnnouncementBar: React.FC<AnnouncementBarProps> = ({ message }) => {
+  return (
+    <div
+      style={{
+        zIndex: 1,
+        backgroundColor: "#3B82F6", // Tailwind's blue-500
+        color: "white",
+        overflow: "hidden",
+        whiteSpace: "nowrap",
+        position: "relative",
+        width: "100vw",
+        left: "50%",
+        transform: "translateX(-50%)",
+      }}
+    >
+      <div
+        style={{
+          display: 'inline-block',
+          paddingLeft: "100%",
+          animation: "marquee 15s linear infinite",
+        }}
+      >
+       📢 {message} 💻
+      </div>
+      <style jsx>{`
+      
+        @keyframes marquee {
+          0% {
+            transform: translateX(100%);
+          }
+          100% {
+            transform: translateX(-100%);
+          }
+        }
+      `}</style>
+    </div>
+  );
+};
+
+export default AnnouncementBar;
